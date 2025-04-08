2bc493c790ced384cf33f8bfe510040e4dcf1890
Intrepid-penguin <dotmycodeprojects@gmail.com>
2025-04-09 00:06:39 +0100

    Update  allowd host settings.py

--- m_journal/settings.py
+++ m_journal/settings.py
@@ -53,7 +53,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['*', 'https://mjournal-production.up.railway.app']
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1,[::1]").split(",")
 
 
 # Application definition
