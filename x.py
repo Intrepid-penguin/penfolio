3b7fbe9ea3fa2a8eeb81aa14a1367b5febdc760f
lftobs <tobbytobs1@gmail.com>
2025-08-28 16:24:41 +0100

    chore: comment --user-data-dir option

--- x.py
+++ x.py
@@ -53,7 +53,7 @@ def scrape_nitter(username='Lf_tobs', max_scrolls=5, proxy=None):
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-gpu')
-    options.add_argument(f'--user-data-dir={temp_dir}')
+    # options.add_argument(f'--user-data-dir={temp_dir}')
     options.add_argument('--remote-debugging-port=9222')
 
     options.add_argument('--disable-setuid-sandbox')
